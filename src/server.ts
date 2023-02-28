b098f5cf8d7bbe3cdc953829fee55210708d06f5
miqueiasbelfort <kawasousa1554@gmail.com>
2023-02-28 10:24:48 -0300

    superchat server

---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config'
+
+import express from 'express'
+import cors from 'cors'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+
+// interfaces
+type Data = {
+    room: string,
+    author: string,
+    message: string,
+    time: string
+}
+
+app.use(cors())
+
+//create server with http
+const serverHttp = http.createServer(app)
+
+// config of socker server
+const io = new Server(serverHttp, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ["GET", "POST"]
+    }
+})
+
+// Connection created when user enters page
+io.on("connection", socket => {
+
+    //console.log(socket.id)
+
+    // Receive an action when the user chooses the room and then creates the room!
+    socket.on("join_room", (roomName: string) => socket.join(roomName))
+    socket.on("leave_room", roomName => socket.leave(roomName))
+
+    // receiving a message
+    socket.on("send_message", (data: Data) => {
+
+        // Send a message back
+        socket.to(data.room).emit("receive_message", data)
+
+    })
+
+    // When the user exits
+    io.on("disconnect", (roomName) => {
+        socket.leave(roomName)
+        //console.log(`User Disconnected: ${socket.id}`)
+    })
+})
+
+serverHttp.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`))
